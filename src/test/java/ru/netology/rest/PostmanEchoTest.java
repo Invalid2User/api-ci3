72650b7877c174747bb991d96008a1de392785b4
Invalid2User <lapina.gen@gmail.com>
2023-03-12 14:03:06 +0300

    Fixed to success

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -16,7 +16,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("no Data"))
+                .body("data", equalTo("Data"))
                 .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
     }
 }
