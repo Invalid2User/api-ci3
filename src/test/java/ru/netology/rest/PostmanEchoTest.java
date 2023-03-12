f2c9569cf3e24c6316fbc78f64477bb7b30b07ac
Invalid2User <lapina.gen@gmail.com>
2023-03-12 13:59:47 +0300

    Initial commit with fail

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class PostmanEchoTest {
+    @Test
+    void checkPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("no Data"))
+                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
+    }
+}
